1fe18411cfde22428f7a9708570c32ae01d49540
trenchesdeveloper <opeyemisamuel222@gmail.com>
2024-06-22 20:29:29 +0100

    get multiple transform images

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
@@ -48,27 +49,70 @@ func main() {
 
 		ext := filepath.Ext(header.Filename)[1:]
 		_ = ext
-		output, err := primitive.Transform(file, ext, 50, primitive.WithMode(primitive.ModeCombo))
+
+		a, err := genImage(file, ext, 100, primitive.ModeTriangle)
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		outFile, err := tempfile("", ext)
+		file.Seek(0, 0)
+
+		b, err := genImage(file, ext, 100, primitive.ModeRect)
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		defer outFile.Close()
+		file.Seek(0, 0)
 
-		io.Copy(outFile, output)
+		c, err := genImage(file, ext, 100, primitive.ModeEllipse)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-		redirectUrl := fmt.Sprintf("/img/%s", filepath.Base(outFile.Name()))
+		file.Seek(0, 0)
 
-		http.Redirect(w, r, redirectUrl, http.StatusFound)
+		d, err := genImage(file, ext, 100, primitive.ModeCircle)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		file.Seek(0, 0)
+		e, err := genImage(file, ext, 100, primitive.ModeRotatedRect)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		file.Seek(0, 0)
+		f, err := genImage(file, ext, 100, primitive.ModeBeziers)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		file.Seek(0, 0)
+		g, err := genImage(file, ext, 100, primitive.ModeRotatedEllipse)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		html := `<html><body>
+			{{range .}}
+				<img src="/{{.}}" />
+			{{end}}
+			</body></html>`
+		templ := template.Must(template.New("").Parse(html))
+
+		images := []string{a, b, c, d, e, f, g}
+
+		templ.Execute(w, images)
 
 	})
 
@@ -76,6 +120,28 @@ func main() {
 
 	log.Fatal(http.ListenAndServe(":3000", mux))
 
+}
+
+func genImage(file io.Reader, ext string, numShapes int, mode primitive.Mode) (string, error) {
+	output, err := primitive.Transform(file, ext, numShapes, primitive.WithMode(mode))
+
+		if err != nil {
+			return "",err
+
+		}
+
+		outFile, err := tempfile("", ext)
+
+		if err != nil {
+
+			return "", err
+		}
+
+		defer outFile.Close()
+
+		io.Copy(outFile, output)
+
+		return outFile.Name(), nil
 }
 
 func tempfile(prefix, ext string) (*os.File, error) {
